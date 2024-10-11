906b4c646bbb355f2d560eafa1a6c177c3bb0151
Mauricio Corte <Corte.Mauricio98@gmail.com>
2024-10-11 13:06:39 +0200

    Commiting things in test branch

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Anton. I just changed this line!");
+            Console.WriteLine("Hello Anton. I just changed this line!");
+            Console.WriteLine("Hello Anton. I just changed this line!");
         }
     }
 }
